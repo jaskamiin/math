f855a5831a19cf01d8142e5e6d5f6402ad825dbe
jaska jaskaalainen <jaskamiin@gmail.com>
2015-02-21 18:05:03 -0500

    add file to test and benchmark bisection function

---
+++ bisect_test.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <benchmark.h>
+
+typedef float (*func)(float);
+
+//test function: x^5 + 7x^4 - 8x^3 + x^2 - 2
+float test(float x){
+	return (x*x*x) - (x*x) - x - 2;
+}
+
+//function to perform bisection method
+float bisect(float(*f)(float), float e, float a, float b){
+	//use intermediate value theorem to check 
+	//if a root exists in interval [a,b]
+	if (f(a)*f(b) > 0) { printf("Invalid interval - returning\n");  return 0; }
+
+	float c;
+	while (1){
+		c = (a + b) / 2;
+		if ((b-c)/c<=e) break;
+		f(b)*f(c) <= 0 ? a = c : b = c;
+	}
+	return c;
+}
+
+//test method with function f(x), e = 10^-4, on [1,2]
+int main(){
+	func f = &test;
+	double start, end, ans;
+
+	start = get_time();
+	ans = bisect(f, .00001f, 1, 2);
+	end = get_time();
+
+	printf("The root is %f and took %e seconds", ans, end-start);
+	return 0;
+}
