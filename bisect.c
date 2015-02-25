e51d6a9bde93a361f28cd391b6b22168a525ddf6
jaska jaskaalainen <jaskamiin@gmail.com>
2015-02-25 04:42:23 -0500

    add better checks to bisect.c

--- bisect.c
+++ bisect.c
@@ -1,12 +1,14 @@
-﻿float bisect(float(*f)(float), float e, float a, float b){
-	//use intermediate value theorem to check 
-	//if a root exists in interval [a,b]
-	if (f(a)*f(b) > 0) return 0; 
+﻿double bisect(double(*f)(double), double eps, double a, double b){
+	//ensure interval is a < b, epsilon > 0 
+	//and that a root exists in [a,b] using the intermediate value theorem
+	if (b < a){ double temp = a; a = b; b = temp; }
+	if (eps <= 0) return 0;
+	if (f(a)*f(b) > 0) { printf("Invalid interval - returning\n");  return 0; }
 
-	float c;
-	while (1){
+	double c;
+	while (1){	
 		c = (a + b) / 2;
-		if ((b-c)/c<=e) break;
+		if ((b - c)/2 <= eps) break;
 		f(b)*f(c) <= 0 ? a = c : b = c;
 	}
 	return c;
