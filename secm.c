6a5080a344d8a00e08e7f329cdafb297ae2dd618
jaska jaskaalainen <jaskamiin@gmail.com>
2015-02-25 04:06:17 -0500

    add secant method

---
+++ secm.c
@@ -0,0 +1,9 @@
+﻿double secm(double(*f)(double), double a, double b){
+	double c;
+	for (int i = 0; i < 1000; i++){
+		if (f(a) == f(b)) break;
+		c = a - f(a) * (a - b) / (f(a) - f(b));
+		b = a; a = c;
+	}
+	return c;
+}
