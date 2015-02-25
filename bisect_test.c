74c41af2a6f77b1a12025aa101a925defc43b512
jaska jaskaalainen <jaskamiin@gmail.com>
2015-02-25 01:49:18 -0500

    add more precision to the output

--- bisect_test.c
+++ bisect_test.c
@@ -1,23 +1,28 @@
 ﻿#include <stdio.h>
+#include <time.h>
 #include <benchmark.h>
+#include <float.h>
 
-typedef float (*func)(float);
+typedef double (*func)(double);
 
-//test function: x^3 - x^2 - x - 2
-float test(float x){
-	return (x*x*x) - (x*x) - x - 2;
+//test function: x^5 + 7x^4 - 8x^3 + x^2 - 2
+double test(double x){
+	return (x*x*x) + (4*x) - 10;
 }
 
 //function to perform bisection method
-float bisect(float(*f)(float), float e, float a, float b){
-	//use intermediate value theorem to check 
-	//if a root exists in interval [a,b]
+double bisect(double(*f)(double), double eps, double a, double b){
+
+	//ensure interval is a < b, epsilon > 0 
+	//and that a root exists in [a,b] using the intermediate value theorem
+	if (b < a){ double temp = a; a = b; b = temp; }
+	if (eps <= 0) return 0;
 	if (f(a)*f(b) > 0) { printf("Invalid interval - returning\n");  return 0; }
 
-	float c;
-	while (1){
+	double c;
+	while (1){	
 		c = (a + b) / 2;
-		if ((b-c)/c<=e) break;
+		if ((b - c)/2 <= eps) break;
 		f(b)*f(c) <= 0 ? a = c : b = c;
 	}
 	return c;
@@ -29,9 +34,10 @@ int main(){
 	double start, end, ans;
 
 	start = get_time();
-	ans = bisect(f, .00001f, 1, 2);
+	ans = bisect(f, .00000000000000000000001l, 1, 2);
 	end = get_time();
 
-	printf("The root is %f and took %e seconds", ans, end-start);
+	printf("The root is %.*g and took %e seconds", DECIMAL_DIG*2, ans, end-start);
+	getchar();
 	return 0;
 }
