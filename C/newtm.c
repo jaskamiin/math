602d94e170a7f8f52cf6a860bbede4ff00905783
jaska jaskaalainen <jaskamiin@gmail.com>
2015-03-11 18:03:56 -0400

    hopefully fix weird indentations?

--- C/newtm.c
+++ C/newtm.c
@@ -1,20 +1,20 @@
 ﻿//REALLY basic function to find a simple derivative
 static inline double diff(double(*f)(double), double x0){
 	const double delta = 1.0e-6; // or similar
-	double	x1 = x0 - delta, 
-			x2 = x0 + delta,
-			y1 = f(x1),
-			y2 = f(x2);
+	double  x1 = x0 - delta, 
+            x2 = x0 + delta,
+            y1 = f(x1),
+            y2 = f(x2);
 
-	return (y2 - y1) / (x2 - x1);
+    return (y2 - y1) / (x2 - x1);
 }
 
 //newton method implementation
 double newton_method(double(*f)(double), double a){
-	for (int i = 0; i < 1000; i++){
+    for (int i = 0; i < 1000; i++){
         double dA = diff(f, a), fA = f(a);
-		if (fA == dA) break; 
+        if (fA == dA) break; 
         if (fabs(fA - dA) <= 0.00005f) break;
-		a = a - fA / dA;
-	}
+        a = a - fA / dA;
+    }
 }
